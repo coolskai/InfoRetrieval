fef2ad7c2a91dba8b23d01d5a7abc2656c3b67f6
bksun <bksun@vision.hoseo.edu>
2020-01-26 18:41:53 +0900

    test commit

--- BooleanTest.py
+++ BooleanTest.py
@@ -30,3 +30,5 @@ if __name__ == '__main__':
         print('doc#', document, '.', end='')
         print(documents[document].strip(), ' : ', end='')
         print(bm.generate_document_vector(document))
+
+    print('test')
